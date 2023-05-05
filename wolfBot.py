a2e17ecb9cd0579be9c1b27003b0e7febefc23ce
proaro2001 <proaro2001@gmail.com>
2023-05-04 19:47:08 -0700

    initalize

---
+++ wolfBot.py
@@ -0,0 +1,24 @@
+BOT_TOKEN = "MTEwMzg3NDcyNjIwMTQwNTU4MA.GYTYnE.Kd71rBAZowAm76j77NycXEKsnV4Q04xzfJNlfs"
+CHANNEL_ID = 1100690193012506635
+
+from discord.ext import commands
+import discord
+
+# declare variables
+command_prefix = "/"                # what should the command start with
+intents = discord.Intents.all()     # Intents to use all functions
+
+bot = commands.Bot(command_prefix=command_prefix, intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Werewolf Bot activated!")
+    # trying to send a message in specific channel
+    # channel ID is needed
+    # not sure if we could create a new one and extract it's channel ID tho
+    channel = bot.get_channel(CHANNEL_ID)
+    # await channel.send("Study Bot activated! (Test)")
+
+
+if __name__ == '__main__':
+    bot.run(BOT_TOKEN)
