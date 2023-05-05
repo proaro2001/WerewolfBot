e717814c68e89e7ff3774f1eef5f605a719151fa
proaro2001 <proaro2001@gmail.com>
2023-05-04 20:43:19 -0700

    gameState

--- wolfBot.py
+++ wolfBot.py
@@ -4,6 +4,22 @@ CHANNEL_ID = 1100690193012506635
 from discord.ext import commands
 import discord
 
+# gameState ( temporary ), global variable
+gameState = {
+    """
+    Role : User
+    """
+    # Village Team
+    "Seer": None,
+    "Witch": None,
+    "Village1": None,
+    "Village2": None,
+
+    # Werewolf Team
+    "Werewolf1": None,
+    "Werewolf2": None
+}
+
 # declare variables
 command_prefix = "/"                # what should the command start with
 intents = discord.Intents.all()     # Intents to use all functions
@@ -27,9 +43,19 @@ async def draw(ctx):
     # 2) send private message to this player
     await send_private_message( user, "Hi ")
 
+
+###############################################
+#              helper methods
+###############################################
+
+# helper method to send private message to user
 async def send_private_message(user, message):
     dm_channel = await user.create_dm()
     await dm_channel.send(message)
 
+
+
+
 if __name__ == '__main__':
     bot.run(BOT_TOKEN)
+
